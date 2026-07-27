50b60b1f18c0f7021abae4995624e100f0a98b10
DanielGengel <daniel.gengel@outlook.com>
2026-07-27 18:20:49 +0200

    Added fonts and all templates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,29 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header-component/header-component';
+import { FooterComponent } from './layout/footer-component/footer-component';
+import { HeroComponent } from './sections/hero-component/hero-component';
+import { AboutComponent } from './sections/about-component/about-component';
+import { SkillsComponent } from './sections/skills-component/skills-component';
+import { PortfolioComponent } from './sections/portfolio-component/portfolio-component';
+import { ReferencesComponent } from './sections/references-component/references-component';
+import { ContactComponent } from './sections/contact-component/contact-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    HeroComponent,
+    AboutComponent,
+    SkillsComponent,
+    PortfolioComponent,
+    ReferencesComponent,
+    ContactComponent,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('portfolio');
