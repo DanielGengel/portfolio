e689a284a76a657781fe509b547c4942ef17712d
DanielGengel <daniel.gengel@outlook.com>
2026-07-28 14:54:08 +0200

    Feat: Skills section added, section titles moved to styles.scss

--- src/app/sections/skills-component/skills-component.ts
+++ src/app/sections/skills-component/skills-component.ts
@@ -1,9 +1,53 @@
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills-component',
   imports: [],
   templateUrl: './skills-component.html',
-  styleUrl: './skills-component.scss',
+  styleUrl: './skills-component.scss'
 })
-export class SkillsComponent {}
+export class SkillsComponent {
+  readonly skills = [
+    {
+      name: 'Angular',
+      icon: '/assets/icons/angular-icon.png'
+    },
+    {
+      name: 'TypeScript',
+      icon: '/assets/icons/ts-icon.png'
+    },
+    {
+      name: 'JavaScript',
+      icon: '/assets/icons/js-icon.png'
+    },
+    {
+      name: 'HTML',
+      icon: '/assets/icons/html-icon.png'
+    },
+    {
+      name: 'CSS',
+      icon: '/assets/icons/css-icon.png'
+    },
+    {
+      name: 'Supabase',
+      icon: '/assets/icons/supabase-icon.png'
+    },
+    {
+      name: 'Git',
+      icon: '/assets/icons/git-icon.png'
+    },
+    {
+      name: 'Scrum',
+      icon: '/assets/icons/scrum-icon.png'
+    },
+    {
+      name: 'REST API',
+      icon: '/assets/icons/rest-api-icon.png'
+    },
+    {
+      name: 'Material Design',
+      icon: '/assets/icons/material-icon.png'
+    }
+  ];
+}
