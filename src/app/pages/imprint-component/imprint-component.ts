f5618f585910241f22c1e5ecc755a236741a3d62
DanielGengel <daniel.gengel@outlook.com>
2026-08-03 15:08:59 +0200

    Feat: added translation to footer, imprint and privacy policy

--- src/app/pages/imprint-component/imprint-component.ts
+++ src/app/pages/imprint-component/imprint-component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {TranslatePipe} from '@ngx-translate/core';
 import { HeaderComponent } from '../../layout/header-component/header-component';
 import { FooterComponent } from '../../layout/footer-component/footer-component';
 @Component({
   selector: 'app-imprint-component',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, TranslatePipe],
   templateUrl: './imprint-component.html',
   styleUrl: './imprint-component.scss',
 })
--- src/app/pages/privacy-component/privacy-component.ts
+++ src/app/pages/privacy-component/privacy-component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-privacy-component',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './privacy-component.html',
   styleUrl: './privacy-component.scss',
 })
